836d7eeb1b1f28a3d3c03476a06c18561c25bdd9
lemmes33 <vinlemuel021@gmail.com>
2024-08-06 14:29:19 +0300

    Programs added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,13 +4,14 @@ import Footer from './components/Footer/Footer';
 import Training from './components/PersonalTraining/Training';
 import Home from './components/Home/Home'
 import Trainers from './components/Trainers/Trainers';
+import Programs from './components/Programs/Programs'
 import './App.css'; // Import stylesheet
 
 function App() {
   return (
     <div className="App">
       <section id="home"><Home /></section>
-      
+      <section id="programs"><Programs /></section>
       <section id="training"><Training /></section>
       <section id="trainers"><Trainers /></section>
       <section id="payment"><Payment /></section>
