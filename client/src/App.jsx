5fc6dd923bbf88a199e567e4862bbb987fe095f3
lemmes33 <vinlemuel021@gmail.com>
2024-08-06 13:57:46 +0300

    Trainers added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 import Payment from './components/Payments/payment';
 import Footer from './components/Footer/Footer';
 import Training from './components/PersonalTraining/Training';
-
 import Home from './components/Home/Home'
+import Trainers from './components/Trainers/Trainers';
 import './App.css'; // Import stylesheet
 
 function App() {
   return (
     <div className="App">
       <section id="home"><Home /></section>
+      
       <section id="training"><Training /></section>
+      <section id="trainers"><Trainers /></section>
       <section id="payment"><Payment /></section>
       
       <section id="footer"><Footer /></section>
