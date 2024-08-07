a20ac90038d5b6ed2c80726fdd5849c28ece3897
lemmes33 <vinlemuel021@gmail.com>
2024-08-07 14:35:40 +0300

    Updated again

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,49 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-import HomePage from './pages/home'
-import LoginPage from './pages/login'
+// import HomePage from './pages/home'
+// import LoginPage from './pages/login'
+
+import Payment from './components/Payments/payment';
+import Footer from './components/Footer/Footer';
+import Training from './components/PersonalTraining/Training';
+import Home from './components/Home/Home'
+import Trainers from './components/Trainers/Trainers';
+import Programs from './components/Programs/Programs'
 
 import './App.css'; // Import stylesheet
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <HomePage />
-  }, {
-    path: '/login',
-    element: <LoginPage />
-  }
-])
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <HomePage />
+//   }, {
+//     path: '/login',
+//     element: <LoginPage />
+//   }
+// ])
 
 function App() {
   return (
-    <RouterProvider router={router} />
+    <div className="App">
+      <section id="home">
+        <Home />
+      </section>
+      <section id="programs">
+        <Programs />
+      </section>
+      <section id="training">
+        <Training />
+      </section>
+      <section id="payment">
+        <Payment />
+      </section>
+      <section id="trainers">
+        <Trainers />
+      </section>
+      <section id="footer">
+        <Footer />
+      </section>
+    </div>
   );
 }
 
