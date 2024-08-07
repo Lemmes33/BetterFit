ce4d0cff8616f2dbdec953dbecee448d22dbf164
Obala <oballasteve4@gmail.com>
2024-08-07 16:18:09 +0300

    updates the nutrition page

--- client/src/components/Nutrition/Nutrition.jsx
+++ client/src/components/Nutrition/Nutrition.jsx
@@ -1,20 +1,48 @@
-// Nutrition.jsx
 import React from 'react';
-import Dishes from './Dishes'; // Make sure the path is correct based on your file structure
 import './Nutrition.css';
+
+const NutritionCard = ({ imageUrl, title, author, description }) => {
+  return (
+    <div className="card">
+      <div className="image" style={{ backgroundImage: `url(${imageUrl})` }}></div>
+      <div className="content">
+        <div className="header">{title}</div>
+        <div className="meta">By {author}</div>
+        <div className="description">{description}</div>
+      </div>
+    </div>
+  );
+};
 
 const Nutrition = () => {
   return (
     <div>
-      <h1>NUTRITION PLAN</h1>
-      <div className="infobox">
-      <h2></h2>
-      <p><h3>Embarking on a gym journey can be significantly enhanced by embracing our healthy meal plan. Designed to complement your fitness goals, our meal plan provides balanced and nutritious options that fuel your workouts and aid in recovery. By enjoying a variety of wholesome foods that support muscle growth, boost energy, and improve overall performance, you'll find that reaching your fitness targets becomes more attainable and enjoyable. Our meal plan is crafted to ensure that you not only meet your dietary needs but also savor every bite, making your fitness journey both rewarding and delicious. Embrace our healthy meal plan to fuel your progress and achieve your gym goals with enthusiasm and vitality.</h3></p>
+      <h1>NUTRIT<span>ION</span> <span>PLAN</span></h1>
+     <div className='h3'><h3>Meal planning saves time, reduces stress, and helps maintain a balanced diet by allowing you to make healthier food choices in advance.<br/> It minimizes food waste and supports consistent eating habits, which can aid in weight management. By controlling portion sizes and meal composition, meal planning ,can help<br/> prevent overeating and promote weight loss or maintenance. Additionally, it ensures you have nutritious options readily available, reducing the temptation to opt for unhealthy, high-calorie foods.</h3></div>
+
+    <div className="nutrition-container">
       
+      <NutritionCard
+        imageUrl="https://i.pinimg.com/564x/bf/47/ab/bf47ab92ec9e228d30da521926bf0d8a.jpg"
+        title="Vegan Diet"
+        author="Mubanga"
+        description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis maiores expedita exercitationem rerum, voluptate laudantium nostrum ratione aliquam adipisci, vitae soluta repellat cupiditate, inventore ipsa distinctio quidem assumenda voluptates eligendi."
+      />
+      <NutritionCard
+        imageUrl="https://i.pinimg.com/564x/48/ff/38/48ff38738c9f8251a0ce7ecec4d58265.jpg"
+        title="Gluten-Free Diet"
+        author="Mubanga"
+        description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis maiores expedita exercitationem rerum, voluptate laudantium nostrum ratione aliquam adipisci, vitae soluta repellat cupiditate, inventore ipsa distinctio quidem assumenda voluptates eligendi."
+      />
+      <NutritionCard
+        imageUrl="https://i.pinimg.com/564x/ca/5d/e9/ca5de9e75f0873f3d38f56fddd854b55.jpg"
+        title="Protein-Based Diet"
+        author="Mubanga"
+        description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis maiores expedita exercitationem rerum, voluptate laudantium nostrum ratione aliquam adipisci, vitae soluta repellat cupiditate, inventore ipsa distinctio quidem assumenda voluptates eligendi."
+      />
     </div>
-      <Dishes />
     </div>
   );
-}
+};
 
 export default Nutrition;
