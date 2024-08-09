ac8b563bd36393991e1f58dffb60785fb5bd589c
Sharon <Sharonaquilla28@gmail.com>
2024-08-09 22:39:49 +0300

    app.py changes

--- server/app.py
+++ server/app.py
@@ -1,11 +1,25 @@
-from flask import Flask
+from flask import Flask, request, jsonify, session
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_bcrypt import Bcrypt
+from flask_cors import CORS
+from flask_restful import Api, Resource
+from dotenv import load_dotenv
+import os
+from cryptography.fernet import Fernet
+
+from models import db, User, WorkoutPlan, NutritionPlan, ProgressTracking
+
+load_dotenv()
 
 app = Flask(__name__)
-
-# Configure the SQLite database (you can use other databases as well)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///your_database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'your_secret_key'
+app.config['SESSION_TYPE'] = 'filesystem'
 
-# Initialize the database
-db = SQLAlchemy(app)
+db.init_app(app)
+bcrypt = Bcrypt(app)
+migrate = Migrate(app, db)
+CORS(app, supports_credentials=True)
+api = Api(app) 
