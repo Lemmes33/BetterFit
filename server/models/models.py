a6cf99778c0095dd7dacb23fe461862b4fd0a8da
Sharon <Sharonaquilla28@gmail.com>
2024-08-16 10:33:17 +0300

    adds back changes

--- server/models/models.py
+++ server/models/models.py
@@ -1,6 +1,6 @@
 from datetime import date
 from sqlalchemy.orm import validates
-from app import db 
+from models import db 
 
 user_workout_plan = db.Table('user_workout_plan',
     db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
@@ -14,13 +14,11 @@ class User(db.Model):
     email = db.Column(db.String, unique=True, nullable=False)
     password = db.Column(db.String, nullable=False)
     age = db.Column(db.Integer, nullable=False)
-    trainer_id = db.Column(db.Integer, db.ForeignKey('trainer.id'), nullable=True)
 
     # Relationships
     workout_plans = db.relationship('WorkoutPlan', secondary=user_workout_plan, backref=db.backref('users', lazy=True))
     nutrition_plans = db.relationship('NutritionPlan', backref='user', lazy=True)
     progress_tracking = db.relationship('ProgressTracking', backref='user', lazy=True)
-    trainer = db.relationship('Trainer', backref='users', lazy=True)
 
     @validates('email')
     def validate_email(self, key, address):
@@ -39,8 +37,6 @@ class User(db.Model):
             "email": self.email,
             "age": self.age,
             "nationality": self.nationality,
-            "description": self.description,
-            "hobbies": self.hobbies
         }
     
 class WorkoutPlan(db.Model):
@@ -51,7 +47,6 @@ class WorkoutPlan(db.Model):
     duration = db.Column(db.Integer, nullable=False)
     start_date = db.Column(db.Date, nullable=False, default=date.today)
     end_date = db.Column(db.Date, nullable=False)
-    trainer_id = db.Column(db.Integer , db.ForeignKey('trainer.id'), nullable=True)
 
     @validates('duration')
     def validate_duration(self, key, duration):
@@ -104,31 +99,3 @@ class ProgressTracking(db.Model):
             "date": self.date
         }
     
-class Trainer(db.Model):
-   
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String, nullable=False)
-    email = db.Column(db.String, unique=True, nullable=False)
-    phone = db.Column(db.String, nullable=True)
-    specialty = db.Column(db.String, nullable=True)
-    bio = db.Column(db.Text, nullable=True)
-
-    # Relationships
-    workout_plans = db.relationship('WorkoutPlan', backref='trainer', lazy=True)
-    users = db.relationship('User', backref='trainer', lazy=True)
-
-
-    @validates('email')
-    def validate_email(self, key, address):
-        assert '@' in address, "Provided email is invalid"
-        return address
-
-    def to_dict(self):
-        return {
-            "id": self.id,
-            "name": self.name,
-            "email": self.email,
-            "phone": self.phone,
-            "specialty": self.specialty,
-            "bio": self.bio
-        }
